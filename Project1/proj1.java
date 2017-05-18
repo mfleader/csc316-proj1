f86fffbb871201d700e637c2a56d8b665081388b
Matthew F. Leader <mfleader@ncsu.edu>
2017-05-18 13:48:19 -0400

    proj1 first commit

---
+++ Project1/proj1.java
@@ -0,0 +1,13 @@
+public class proj1 {
+
+    private class LinkedList {
+
+        private Node head;
+
+        public class Node {
+
+            private String data;
+            private Node next;
+        }
+    }
+}
